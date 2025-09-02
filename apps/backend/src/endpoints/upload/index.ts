3211494895ab90c4432363fbb2348229e53cc4e1
etonee123x <etonee123x@gmail.com>
2025-09-02 23:17:55 +0400

    backend refactoring

--- apps/backend/src/endpoints/upload/index.ts
+++ apps/backend/src/endpoints/upload/index.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import busboy from 'busboy';
 import { handler } from './handlers';
 import { checkAuth } from '@/middleware';
-import { pick } from '@etonee123x/shared';
 
 const router = Router();
 
@@ -10,7 +9,7 @@ router.post('/', checkAuth, (request, response) => {
   const paths: Array<string> = [];
 
   request.pipe(
-    busboy(pick(request, ['headers']))
+    busboy({ headers: request.headers })
       .on('file', (...args) => paths.push(handler(...args)))
       .on('close', () => response.send(paths)),
   );
