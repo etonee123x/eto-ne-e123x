3cebdd34f592bf5229fa307484924b1771975b63
etonee123x <etonee123x@gmail.com>
2025-09-03 11:12:01 +0400

    vue-i18n fixes

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -21,6 +21,7 @@ module.exports = {
         NODE_ENV: "production",
         APP_ENV: "test",
         INTERNAL_API_URL: "http://127.0.0.1:4001",
+        VITE_API_PREFIX: '/api'
       },
     },
   ],
