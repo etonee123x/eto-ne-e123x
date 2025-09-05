10bed2f3cf610a7e08b8dd34fb7f8ceddfbce6fb
etonee123x <etonee123x@gmail.com>
2025-09-05 11:41:25 +0400

    fixes

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -23,8 +23,6 @@ export default defineConfig(() => {
       tailwindcss(),
     ],
     publicDir: 'public',
-    // Костыль, какие то глубокие импорты, ещё какая то хрень. TODO: заменить vue-i18n --> i18n-next
-    ssr: { noExternal: ['vue-i18n'] },
   };
 
   if (isAppModeDevelopment) {
