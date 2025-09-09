99468ab02b30a0ab3f7df3e0df65211af21018f4
etonee123x <etonee123x@gmail.com>
2025-09-09 23:45:00 +0400

    QoL++

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -6,17 +6,6 @@ import { i18n } from '@/i18n';
 import App from '@/App.vue';
 import { isNotNil } from '@etonee123x/shared/utils/isNotNil';
 
-import { toTemporalInstant, type Temporal } from '@js-temporal/polyfill';
-
-// TODO убрать после стабилизации Temporal и Intl.DurationFormat
-declare global {
-  interface Date {
-    toTemporalInstant(): Temporal.Instant;
-  }
-}
-
-Date.prototype.toTemporalInstant = toTemporalInstant;
-
 export const createApp = (context: Partial<{ url: string }> = {}) => {
   const app = createSSRApp(App);
 
