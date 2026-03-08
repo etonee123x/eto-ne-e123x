7b11dd7dee519634c9ab4c4d5f0baecec4e9fd70
etonee123x <etonee123x@gmail.com>
2026-03-08 22:42:44 +0400

    QOl++-_

--- apps/frontend/src/components/TheFooter.vue
+++ apps/frontend/src/components/TheFooter.vue
@@ -1,8 +1,9 @@
 <template>
   <footer class="border-t border-t-primary-500">
-    <address class="layout-container py-2 flex gap-2 items-center">
-      <a target="_blank" href="https://t.me/eto_ne_e123x">
+    <address class="layout-container py-4 flex gap-2 items-center italic">
+      <a rel="noopener noreferrer" target="_blank" :href="`https://t.me/${ETO_NE_E123X}`" class="flex gap-2">
         <img class="size-6" src="@/assets/icons/telegram.svg" :alt="t('telegramLogo')" />
+        {{ ETO_NE_E123X }}
       </a>
     </address>
   </footer>
@@ -22,4 +23,6 @@ const { t } = useI18n({
     },
   },
 });
+
+const ETO_NE_E123X = 'eto_ne_e123x';
 </script>
