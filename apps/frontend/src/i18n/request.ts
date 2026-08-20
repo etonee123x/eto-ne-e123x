9792576203fda14927b791560b058161d6ffa40d
etonee123x <etonee123x@gmail.com>
2026-08-20 10:37:04 +0400

    WIP metadata

--- apps/frontend/src/i18n/request.ts
+++ apps/frontend/src/i18n/request.ts
@@ -1,10 +1,19 @@
+import { locale as localeRootParameters } from 'next/root-params';
+
 import { getRequestConfig } from 'next-intl/server';
 import { hasLocale } from 'next-intl';
 import { routing } from './routing';
+import { notFound } from 'next/navigation';
 
-export default getRequestConfig(async ({ requestLocale }) => {
-  const requested = await requestLocale;
-  const locale = hasLocale(routing.locales, requested) ? requested : routing.defaultLocale;
+export default getRequestConfig(async ({ locale }) => {
+  if (!locale) {
+    const localeParameter = await localeRootParameters();
+    if (hasLocale(routing.locales, localeParameter)) {
+      locale = localeParameter;
+    } else {
+      notFound();
+    }
+  }
 
   const messagesModule = (await import(`./messages/${locale}.json`)) as { default: object };
   const messages = messagesModule.default;
